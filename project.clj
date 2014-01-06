e951ddff381a41a7717f477fd3e1a8b6673c1e8e
Walter P Babic <wbabic@mac.com>
2014-01-05 21:43:42 -0700

    updated dependencies

--- project.clj
+++ project.clj
@@ -4,16 +4,20 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1820"] 
-                 [org.clojure/core.async "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:source-paths ["dev"]}}
-  :plugins [[lein-cljsbuild "0.3.2"]]
-  :repositories {
-                 "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"
-                 }
+                 [org.clojure/clojurescript "0.0-2138"] 
+                 [ring "1.2.1"]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.4"]
+                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [com.cemerick/piggieback "0.1.2"]]
+  :plugins [[lein-cljsbuild "1.0.1"]]
+  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   :cljsbuild
   {:builds
    [{:source-paths ["src/async_play/web"]
      :compiler {:output-to "resources/public/js/hello.js"
                 :optimizations :whitespace
-                :pretty-print true}}]})
+                :pretty-print true}}]}
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [org.clojure/java.classpath "0.2.0"]]}})
